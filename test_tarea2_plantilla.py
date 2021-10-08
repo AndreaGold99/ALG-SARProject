a3f7674767732556973feb5457b2bd5f6feb4dac
Elias Urios Alacreu <thebuzzeitor@gmail.com>
2021-10-08 13:07:21 +0200

    tests2

--- test_tarea2_plantilla.py
+++ test_tarea2_plantilla.py
@@ -1,14 +1,73 @@
 def dp_levenshtein_threshold(x, y, th):
-    #TODO
-    return 0
+    currentrow = [0]*(1+len(x))
+    previousrow = [0]*(1+len(x))
+    currentrow[0] = 0
+
+    for i in range(1, len(x) + 1):
+        currentrow[i] = currentrow[i-1] + 1
+    for j in range(1, len(y) + 1):
+        previousrow, currentrow = currentrow, previousrow
+        currentrow[0] = previousrow[0] + 1
+        for i in range(1, len(x) + 1):
+            currentrow[i] = min(currentrow[i-1] + 1,
+                previousrow[i] + 1,
+                previousrow[i-1] + (x[i-1] != y[j-1]))
+        if min(currentrow) > th:
+            return th + 1
+    return min(currentrow[len(x)], th + 1)
 
 def dp_restricted_damerau_threshold(x, y, th):
-    #TODO
-    return 0
+    currentrow = [0]*(1+len(x))
+    previousrow = [0]*(1+len(x))
+    previousrow2 = [0]*(1+len(x))
+    currentrow[0] = 0
+
+    for i in range(1, len(x)+1):
+        currentrow[i] = currentrow[i-1] + 1
+    for j in range(1, len(y)+1):
+        previousrow, currentrow, previousrow2 = currentrow, previousrow2, previousrow
+        currentrow[0] = previousrow[0] + 1
+        for i in range(1, len(x)+1):
+            if i > 1 and j > 1 and x[i-2] == y[j-1] and x[i-1] == y[j-2]:
+                aux = previousrow2[i-2] + 1
+            else:
+                aux = 10000
+            currentrow[i] = min(currentrow[i-1] + 1,
+                previousrow[i] + 1,
+                previousrow[i-1]+(x[i-1] != y[j-1]),
+                aux)
+        if min(currentrow) > th:
+            return th + 1
+    return min(currentrow[len(x)], th + 1)
 
 def dp_intermediate_damerau_threshold(x, y, th):
-    #TODO
-    return 0
+    currentrow = [0]*(1+len(x))
+    previousrow = [0]*(1+len(x))
+    previousrow2 = [0]*(1+len(x))
+    previousrow3 = [0]*(1+len(x))
+    currentrow[0] = 0
+
+    for i in range(1, len(x)+1):
+        currentrow[i] = currentrow[i-1] + 1
+    for j in range(1, len(y)+1):
+        previousrow, currentrow, previousrow2, previousrow3 = currentrow, previousrow3, previousrow, previousrow2
+        currentrow[0] = previousrow[0] + 1
+        for i in range(1, len(x)+1):            
+            currentrow[i] = min(currentrow[i-1] + 1,
+                previousrow[i] + 1,
+                previousrow[i-1]+(x[i-1] != y[j-1]))
+
+            if i > 1 and j > 1 and x[i-2] == y[j-1] and x[i-1] == y[j-2]:
+                currentrow[i] = min (currentrow[i], previousrow2[i-2] + 1)
+            
+            if i > 1 and j > 2 and x[i-2] == y[j-1] and x[i-1] == y[j-3]:
+                currentrow[i] = min (currentrow[i], previousrow3[i-2] + 2)
+                
+            if i > 2 and j > 1 and x[i-3] == y[j-1] and x[i-1] == y[j-2]:
+                currentrow[i] = min (currentrow[i], previousrow2[i-3] + 2)
+    if min(currentrow) > th:
+        return th + 1
+    return min(currentrow[len(x)], th + 1)
 
 test = [
         ("algoritmo","algortimo"),
@@ -41,7 +100,7 @@ algoritmo    algortximo   	 levenshtein  2 restricted  2 intermediate  2
 algoritmo    lagortimo    	 levenshtein  2 restricted  2 intermediate  2
 algoritmo    agaloritom   	 levenshtein  2 restricted  2 intermediate  2
 algoritmo    algormio     	 levenshtein  2 restricted  2 intermediate  2
-acb          ba           	 levenshtein  2 restricted  2 intermediate  2
+acb          ba           	 levenshtein  3 restricted  2 intermediate  2
 thresholds:   2
 algoritmo    algortimo    	 levenshtein  2 restricted  1 intermediate  1
 algoritmo    algortximo   	 levenshtein  3 restricted  3 intermediate  2
